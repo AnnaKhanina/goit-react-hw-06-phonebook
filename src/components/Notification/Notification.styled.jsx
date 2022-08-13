76e33a6c46adb8391015919682abc9291f1c6d7f
AnnaKhanina <khanina.anna85@gmail.com>
2022-08-13 14:38:43 +0300

    commit

---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Warning = styled.p`
+  color: red;${p => p.theme.colors.red};
+`; 
+
+export default Warning;
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  padding: 0 15px;${p => p.theme.space[0]} ${p => p.theme.space[4]}px;
+`;
