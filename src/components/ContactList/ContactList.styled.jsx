12f8b7912e7cef6a9d7fc614d3cd50d0c50e71a2
AnnaKhanina <khanina.anna85@gmail.com>
2022-09-18 11:16:10 +0300

    begin redux

--- src/components/ContactList/ContactList.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const List = styled.ul`
-  display: flex;
-  flex-direction: column;
-  padding: ${p => p.theme.space[3]}px ${p => p.theme.space[4]}px;
-  width: ${p => p.theme.sizes.max};
-`;
---
+++ src/components/Main/Main.styled.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+const MainPage = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 40px;
+  color: #483293;
+
+  & h1 {
+    font-size: 46px;
+    margin: 0 0 10px 0;
+  }
+`;
+
+export default MainPage;
